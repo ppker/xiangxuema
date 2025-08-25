061550fd6dd30740b9510fddd9985bf22e0b1a89
xland <412588801@qq.com>
2025-08-25 09:25:47 +0800

    clear

--- RedRedStar/include/RRS/Color.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-namespace RRS {
-	typedef uint32_t Color;
-	static constexpr inline Color GetColor(unsigned r, unsigned g, unsigned b, unsigned a=255) {
-		return (a << 24) | (r << 16) | (g << 8) | (b << 0);
-	}
-}

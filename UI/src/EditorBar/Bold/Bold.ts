a3adb5b329f0bd41b37d79bde32e0deae1d72417
liulun <xland@live.cn>
2026-09-08 16:59:22 +0800

    Update Bold.ts

--- UI/src/EditorBar/Bold/Bold.ts
+++ UI/src/EditorBar/Bold/Bold.ts
@@ -1,9 +1,17 @@
 import html from "./Bold.html?raw";
 import CtrlBase from "../../CtrlBase";
+import { toggleBold } from "roosterjs-content-model-api";
+import EditorContent from "../../EditorContent/EditorContent";
 
 class Bold extends CtrlBase {
   constructor() {
     super(html);
+  }
+
+  override ready(): void {
+    this.dom.addEventListener("click", () => {
+      toggleBold(EditorContent.editor);
+    });
   }
 }
 
